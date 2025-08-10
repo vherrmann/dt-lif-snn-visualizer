51811a14efcb524f0204509e325bb6b532cbb70f
Valentin Herrmann <me@valentin-herrmann.de>
2025-08-10 18:23:04 +0200

    Added vite lint plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [
+    glsl(),
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
 });
